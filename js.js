3aa9f20ce1622a9a465ca0c7d4427ec7d50dcff8
Veronika Teplova <teplovavo@gmail.com>
2024-09-13 14:10:42 -0700

    beginning exercises

---
+++ js.js
@@ -0,0 +1,28 @@
+//Loop through all numbers from 1 to 100.
+for (let a = 1; a <= 100; a++) {
+   // console.log (a);
+} 
+
+
+//If a number is divisible by 3, log “Fizz.”
+for (let b = 1; b <= 100; b++) {
+    if (b % 3 === 0) {
+     //   console.log ("Fizz");
+    }  else{
+     //   console.log(b);
+    }
+}
+
+
+// If a number is divisible by 5, log “Buzz.”
+
+for (let c = 1; c <= 100; c++) {
+    if (c % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(c);
+    }
+}
+
+
+//If a number is divisible by both 3 and 5, log “Fizz Buzz.”
